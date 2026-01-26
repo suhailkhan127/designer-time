3faab18bb520ac881b1711e767478fb103e89134
suhailkhan127 <suhailkhan127@gmail.com>
2026-01-26 20:24:21 +0500

    1710

---
+++ jeem-projects/1710/js/cufon-replace.js
@@ -0,0 +1,8 @@
+
+Cufon.replace(' .shopping, .mid_inner, .manfact_head', { fontFamily: 'Aparajita', hover:true});
+
+Cufon.replace(' #template_menu ul li a', { fontFamily: 'BastardusSans', hover:true});
+
+Cufon.replace(' .banner strong, .catg_bar ', { fontFamily: 'AvantGarde Bk BT', hover:true});
+
+Cufon.replace(' .price ', { fontFamily: 'Baskerville Win95BT bold', hover:true});
