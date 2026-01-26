01938d5c9e4467dad24bf69b4a95078f07162281
suhailkhan127 <suhailkhan127@gmail.com>
2026-01-26 19:20:36 +0500

    1684

---
+++ jeem-projects/1684/html/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() { 
+	$('ul.sf-menu').superfish({ 
+		delay:       600,                            // one second delay on mouseout 
+		animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+		speed:       'normal',                          // faster animation speed 
+		autoArrows:  false,                           // disable generation of arrow mark-up 
+		dropShadows: false                            // disable drop shadows 
+	});
+	/*************** tooltips  ****************/
+	/* $(".list-services a.tooltips").easyTooltip(); */
+	
+	
+}); 
