87e0912ec1b77242dc3e4fcd88413583983d6bf7
suhailkhan127 <suhailkhan127@gmail.com>
2026-01-27 11:24:16 +0500

    Add files via upload

---
+++ jeem-projects/tm-source/1677/html/js/cufon-replace.js
@@ -0,0 +1,5 @@
+
+
+Cufon.replace('.header_text p, .navigation ul li a', { fontFamily: 'Myriad Pro', hover:true});
+
+Cufon.replace('h1, h2, .left_btn a, .inner_btn a, .header_btn a', { fontFamily: 'Myriad Pro b', hover:true});
